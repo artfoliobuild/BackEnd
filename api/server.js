e77a5f14e8c6dee52e2a03e2eb6e37277b10bea6
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-13 22:54:33 -0600

    Finished register endpoint finished login endpoint finished protectRoute middleware finished jwt for authentication

--- api/server.js
+++ api/server.js
@@ -4,10 +4,82 @@ const cors = require('cors');
 const helmet = require('helmet');
 const postsRouter = require('../routers/postsRouter');
 const commentsRouter = require('../routers/commentsRouter');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const server = express();
 server.use(express.json(), helmet(), cors());
 server.use('/posts', postsRouter);
 server.use('/comments', commentsRouter);
+
+const secret = 'This is not my secret!';
+
+const generateToken = user => {
+  const { username, admin } = user;
+  const payload = {
+    username,
+    admin
+  };
+  const options = {
+    expiresIn: '1h',
+    jwtid: bcrypt.hashSync(user.username, 4),
+    subject: `${user.id}`
+  };
+  return jwt.sign(payload, secret, options);
+};
+
+const protectRoute = (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization;
+    jwt.verify(token, secret, (err, decodedToken) => {
+      err ? res.status(401).json({ message: 'Invalid token!' }) : next();
+    });
+  } else {
+    res.status(403).json({ error: 'FORBIDDEN!!!' });
+  }
+};
+
+server.post('/register', (req, res) => {
+  const creds = req.body;
+  if (
+    !creds.password ||
+    !creds.username ||
+    !creds.Firstname ||
+    !creds.Lastname ||
+    !creds.email
+  ) {
+    res.status(400).json({ error: 'All fields are required!' });
+  }
+  creds.password = bcrypt.hashSync(creds.password, 12);
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      db('users')
+        .where('id', ids[0])
+        .first()
+        .then(user => {
+          const { username, admin } = user;
+          res.status(201).json({ username, admin, token: generateToken(user) });
+        });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.post('/login', (req, res) => {
+  const creds = req.body;
+  db('users')
+    .where('username', creds.username)
+    .first()
+    .then(user =>
+      user && bcrypt.compareSync(creds.password, user.password)
+        ? res.json({
+            username: user.username,
+            admin: user.admin,
+            token: generateToken(user)
+          })
+        : res.status(401).json({ message: 'Invalid username or password!' })
+    )
+    .catch(err => res.status(500).json(err));
+});
 
 module.exports = server;
