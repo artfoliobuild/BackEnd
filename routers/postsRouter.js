6c0f28ee24c5e994a23a4f6d9786b55836256b9e
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-14 13:18:15 -0600

    finished get endpoint for posts finished get by id and join comment table endpoint

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -2,4 +2,26 @@ const express = require('express');
 const router = express.Router();
 const db = require('../dbConfig');
 
+router
+  .get('/', (req, res) => {
+    db('posts')
+      .then(posts =>
+        !posts.length
+          ? res.status(404).json({
+              message: 'There is no post just yet, please try again later!'
+            })
+          : res.json(posts)
+      )
+      .catch(err => res.status(500).json(err));
+  })
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('posts')
+      .where('post.id', id)
+      .join('comments', 'post.id', 'comment.post_id')
+      .then(post => res.json(post))
+      .catch(err => res.status(500).json(err));
+  })
+  .post('/', (req, res) => {});
+
 module.exports = router;
