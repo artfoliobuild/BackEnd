e99d8c1b61db4d1cd3964cc02cb27080fc8cdc25
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-13 19:23:32 -0600

    removed username from posts table's schema

--- data/migrations/20190213123407_posts_table.js
+++ data/migrations/20190213123407_posts_table.js
@@ -1,10 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('posts', table => {
     table.increments();
-    table
-      .string('username', 255)
-      .notNullable()
-      .unique();
     table.string('description');
     table.integer('likes').unsigned();
     table
