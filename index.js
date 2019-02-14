efa752d161a4321a2362e06a26a952245a492543
jeffyjkang <jeffyjkang@gmail.com>
2019-02-13 20:38:46 -0800

    update

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const server = express();
+
+const port = process.env.PORT || 9000;
+
+server.get("/", (req, res) => {
+  res.send("<h1>server running</h1>");
+});
+
+// let port = process.env.PORT;
+// if (port == null || port == "") {
+//   port = 8000;
+// }
+// app.listen(port);
+
+server.listen(port, () => console.log(`api running on port ${port}...`));
