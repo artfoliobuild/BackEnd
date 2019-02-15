760a1b2d631a22d53cb84d4055592b71b408a24f
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-14 20:12:03 -0600

    finished get endpoint for comments

--- routers/commentsRouter.js
+++ routers/commentsRouter.js
@@ -2,4 +2,16 @@ const express = require('express');
 const router = express.Router();
 const db = require('../dbConfig');
 
+router.get('/', (req, res) => {
+  db('comments')
+    .then(comments =>
+      !comments.length
+        ? res.status(404).json({
+            message: 'There is no comment just yet, please try again later!'
+          })
+        : res.json(comments)
+    )
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
