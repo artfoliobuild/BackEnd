0894e10d4aac43958a77b11b0e8489c003d5ee7e
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-14 20:41:32 -0600

    finished get by id endpoint for comments

--- routers/commentsRouter.js
+++ routers/commentsRouter.js
@@ -2,16 +2,32 @@ const express = require('express');
 const router = express.Router();
 const db = require('../dbConfig');
 
-router.get('/', (req, res) => {
-  db('comments')
-    .then(comments =>
-      !comments.length
-        ? res.status(404).json({
-            message: 'There is no comment just yet, please try again later!'
-          })
-        : res.json(comments)
-    )
-    .catch(err => res.status(500).json(err));
-});
+router
+  .get('/', (req, res) => {
+    db('comments')
+      .then(comments =>
+        !comments.length
+          ? res.status(404).json({
+              message: 'There is no comment just yet, please try again later!'
+            })
+          : res.json(comments)
+      )
+      .catch(err => res.status(500).json(err));
+  })
+  .get('/:id', (req, res) => {
+    const { id } = req.params;
+    db('comments')
+      .where('id', id)
+      .then(comment =>
+        comment
+          ? res.json(comment)
+          : res
+              .status(404)
+              .json({
+                message: 'There is no comment with the specified ID, try again!'
+              })
+      )
+      .catch(err => res.status(500).json(err));
+  });
 
 module.exports = router;
