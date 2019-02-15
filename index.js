50ba84631ab3af8e27d9ec7c852611509ea706c1
jeffyjkang <jeffyjkang@gmail.com>
2019-02-15 15:31:18 -0800

    test

--- index.js
+++ index.js
@@ -7,10 +7,4 @@ server.get("/", (req, res) => {
   res.send("<h1>server running</h1>");
 });
 
-// let port = process.env.PORT;
-// if (port == null || port == "") {
-//   port = 8000;
-// }
-// app.listen(port);
-
 server.listen(port, () => console.log(`api running on port ${port}...`));
