179c275b266b09d541f4b015c5d6b154740900a1
jeffyjkang <jeffyjkang@gmail.com>
2019-02-14 23:00:24 -0800

    ci

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+// var less = require("gulp-less");
+// var babel = require("gulp-babel");
+// var concat = require("gulp-concat");
+// var uglify = require("gulp-uglify");
+// var rename = require("gulp-rename");
+// var cleanCSS = require("gulp-clean-css");
+// var del = require("del");
+
+gulp.task("travis", ["build", "testServerJS"], function() {
+  process.exit(0);
+});
