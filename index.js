db016223a041015c65ed66cd3aa5995f43e9d355
Jorlost19 <39849563+Jorlost19@users.noreply.github.com>
2019-02-18 23:03:23 -0600

    fixed typo

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
-const PORT = process.env.PORT || SERVER_PORT;
+require('dotenv').config();
+const PORT = process.env.PORT || 9000;
 
-const server = require("./api/server");
+const server = require('./api/server');
 
-server.get("/", (req, res) => {
-  res.send("<h1>server running</h1>");
+server.get('/', (req, res) => {
+  res.send('<h1>server running</h1>');
 });
 
 server.listen(PORT, console.log(`Server is up and running on port ${PORT}!`));
