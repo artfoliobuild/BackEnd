1ce9753ccaf7270c4d61659162e931dc015ae844
jeffyjkang <jeffyjkang@gmail.com>
2019-02-18 19:22:38 -0800

    update dot env to dev dependencies

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // require('dotenv').config();
-const server = require('./api/server');
+const server = require("./api/server");
 
 const PORT = process.env.SERVER_PORT;
 
-server.get('/', (req, res) => {
-  res.send('<h1>server running</h1>');
+server.get("/", (req, res) => {
+  res.send("<h1>server running</h1>");
 });
 
 server.listen(PORT, console.log(`Server is up and running on port ${PORT}!`));
